ae2ef6907ab2527fee2c7beb8feed996289a071f
Luis Floresgit config --global user.name Luis <Lflores02142@gmail.com>
2019-11-01 11:37:39 -0600

    worte some tests

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,27 +2,29 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import PlayerCard from "./components/PlayerCard"
-import Player from "./components/Players"
-import { render } from "@testing-library/react";
+import Players from "./components/Players"
+import { render, getByTestId } from "@testing-library/react";
 
 test("renders without crashing", () => {
   render(<App />);
 });
-
+test("PlayerCard renders", () => {
+  const { getBytestId } = render(<Players />);
+})
 test("search text renders witout crashing", () =>{
-  const { getByTestId } = render(<PlayerCard />);
-  getByTestId("serachText")
+  const { getByTestId } = render(<PlayerCard /> );
+  getByTestId("searchText")
 } )
 
 test("header displays", () => {
-  const { getByTestId } = render(<Player />);
+  const { getByTestId } = render(<Players />);
   getByTestId("header") 
 })
 test("Player Name", () =>{
-  const { getByTestId } = render(<PlayerCard />);
+  const { getByTestId } = render(<Players />,<PlayerCard />);
   getByTestId("playerName")
 } )
 test("Player Country", () =>{
-  const { getByTestId } = render(<PlayerCard />);
+  const { getByTestId } = render(<Players />,<PlayerCard />);
   getByTestId("playerCountry")
 } )
--- client/src/components/PlayerCard.js
+++ client/src/components/PlayerCard.js
@@ -4,38 +4,37 @@ import Paper from "@material-ui/core/Paper";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles(() => ({
-    root:{
-        flexGrow: 1
-    },
-    paper:{
-        height:100,
-        width:100,
-        padding:40,
-        paddingTop:2,
-        margin:15,
-        marginTop:25
-
-    }
-}))
+  root: {
+    flexGrow: 1
+  },
+  paper: {
+    height: 100,
+    width: 100,
+    padding: 40,
+    paddingTop: 2,
+    margin: 15,
+    marginTop: 25
+  }
+}));
 
 const PlayerCard = props => {
-    const classes = useStyles();
+  const classes = useStyles();
   return (
     <div>
-        <Grid container className={classes.root} spacing={5}>
-            <Grid item xs={12}>
-            <Grid container justify="center" spacing={5}>
-      {props.players.map(player => (
-        <Grid conatiner spacing={5}>
-            <Paper className={classes.paper} >
-          <p data-testid="playerName">{player.name}</p>
-          <p data-testid="playerCountry">{player.country}</p>
-          <p data-testid='searchText' >Searches:{player.searches}</p>
-          </Paper>
+      <Grid container className={classes.root} spacing={5}>
+        <Grid item xs={12}>
+          <Grid container justify="center" spacing={5}>
+            {props.players.map(player => (
+              <Grid conatiner spacing={5}>
+                <Paper className={classes.paper}>
+                  <p data-testid="playerName">{player.name}</p>
+                  <p data-testid="playerCountry">{player.country}</p>
+                  <p data-testid="searchText">Searches:{player.searches}</p>
+                </Paper>
+              </Grid>
+            ))}
           </Grid>
-      ))}
-      </Grid>
-      </Grid>
+        </Grid>
       </Grid>
     </div>
   );
